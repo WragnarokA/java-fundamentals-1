7807c0d3af2996cc67dfaa84d5bfdbb2bb096187
wilmer <wilmer.arango.a@gmail.com>
2023-10-18 16:53:38 +0200

    Biglietto del treno

---
+++ src/fundamentals/uno/java/CalcoloBigliettoDelTreno.java
@@ -0,0 +1,30 @@
+package fundamentals.uno.java;
+import java.text.DecimalFormat;
+import java.util.Scanner;
+public class CalcoloBigliettoDelTreno {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        DecimalFormat df = new DecimalFormat("0.00");
+
+        System.out.print("Inserisci il numero di chilometri da percorrere: ");
+        double km = scanner.nextDouble();
+
+        System.out.print("Inserisci l'età del passeggero: ");
+        int eta = scanner.nextInt();
+
+        double prezzoBase = km * 0.21;
+
+        if (eta < 18) {
+            prezzoBase *= 0.8;
+        } else if (eta >= 65) {
+            prezzoBase *= 0.6;
+        }
+        String numeroArrontondato = df.format(prezzoBase);
+        System.out.println("Il prezzo del biglietto è: " + numeroArrontondato + " €");
+
+        scanner.close();
+    }
+
+
+
+}
